01b5047bf830540d7077766bf68f96388c76c68d
Luciano Peixoto <lucianopeixoto@hotmail.com>
2022-10-31 16:21:29 -0400

    code cleanup

--- options.js
+++ options.js
@@ -4,12 +4,6 @@ function save_options() {
 	localStorage["width"] = $("#width").val();
 	localStorage["height"] = $("#height").val();
 
-	// New storage api:
-	//
-	//chrome.storage.sync.set({localurl: $("#localurl").val()}, function() {
-	//	console.log('Value of localurl was set to ' + $("#localurl").val());
-	//});
-
 	$("#status").html("Options Saved.");
 	setTimeout(function() {
 		$("#status").html("");
@@ -22,12 +16,6 @@ function restore_options() {
 	var width = localStorage["width"];
 	var height = localStorage["height"];
 
-	// New storage api:
-	//
-	//var localurl = chrome.storage.sync.get(['localurl'], function(result) {
-	//	console.log('Value currently is ' + result.key);
-	//});
-	
 	if(localurl){
 		$("#localurl").val(localurl);
 	}
@@ -44,7 +32,6 @@ function restore_options() {
 
 $(document).ready(function(){
 	restore_options();
-	
 	$("#save").click(function(){
 		save_options();
 	});
