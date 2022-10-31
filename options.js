103031a424d7ee70c446eff6bb6ab1350036cca3
Luciano Peixoto <lucianopeixoto@hotmail.com>
2022-10-31 01:20:16 -0400

    Testing new chrome storage api

--- options.js
+++ options.js
@@ -4,6 +4,12 @@ function save_options() {
 	localStorage["width"] = $("#width").val();
 	localStorage["height"] = $("#height").val();
 
+	// New storage api:
+	//
+	//chrome.storage.sync.set({localurl: $("#localurl").val()}, function() {
+	//	console.log('Value of localurl was set to ' + $("#localurl").val());
+	//});
+
 	$("#status").html("Options Saved.");
 	setTimeout(function() {
 		$("#status").html("");
@@ -15,6 +21,12 @@ function restore_options() {
 	var remoteurl = localStorage["remoteurl"];
 	var width = localStorage["width"];
 	var height = localStorage["height"];
+
+	// New storage api:
+	//
+	//var localurl = chrome.storage.sync.get(['localurl'], function(result) {
+	//	console.log('Value currently is ' + result.key);
+	//});
 	
 	if(localurl){
 		$("#localurl").val(localurl);
